5f4747559d24a2910b08a0dd7a31646e263e10e2
Aniket Kumar Verma <vermaaniket213@gmail.com>
2020-09-07 12:24:33 +0530

    Updates

---
+++ precache-manifest.7d0ee7982384ecd763a80c220e416dc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebe019aec0db0c9c60061200e83c9289",
+    "url": "/react_deploy/index.html"
+  },
+  {
+    "revision": "1b56e4915c591c25453d",
+    "url": "/react_deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a430b637cc3d54966e2a",
+    "url": "/react_deploy/static/js/2.0999be9a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_deploy/static/js/2.0999be9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b56e4915c591c25453d",
+    "url": "/react_deploy/static/js/main.164da098.chunk.js"
+  },
+  {
+    "revision": "c1f92b9dcf22317af2ac",
+    "url": "/react_deploy/static/js/runtime-main.61bfaca3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
